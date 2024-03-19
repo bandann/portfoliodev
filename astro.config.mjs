9bd3cde451ae45313dc995fc8f3c0d1c3f47905e
bandann <danielinaga@gmail.com>
2024-03-18 21:17:14 -0400

    astro config update dia 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(),tailwind()],
+  integrations: [tailwind()],
   output: "server",
   adapter: netlify()
 });
