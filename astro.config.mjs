1e6aa9733d2a30b1e02de7532d004a4afaad7d59
bandann <danielinaga@gmail.com>
2024-03-18 21:10:29 -0400

    portfolio dia uno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [icon(),tailwind()],
   output: "server",
   adapter: netlify()
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['selector', '[data-mode="dark"]'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"dark-bg": "#111",
+				"dark-text": "#ddd",
+			  },
+		},
 	},
 	plugins: [],
 }
